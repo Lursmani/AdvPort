f879ad9850fdaf4a6218502b34fa236dd6d08040
Davit <lursmani9@gmail.com>
2026-04-18 22:22:17 +0200

    feat: init project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen w-full">
+      <div className="mx-auto flex min-h-screen w-full max-w-7xl flex-col px-6 py-8 sm:px-10 lg:px-12">
+        <div className="flex-1">
+          <Header />
+        </div>
+      </div>
+    </main>
+  );
+}
